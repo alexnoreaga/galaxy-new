5599c7408daae9ea67bc1dcbc2471823923746ba
unknown <alexnoreaga@gmail.com>
2023-11-02 20:07:50 +0700

    percobaan edit

--- app/components/BottomNavbar.jsx
+++ app/components/BottomNavbar.jsx
@@ -65,7 +65,7 @@ export const BottomNavbar = () => {
                 to='/pages/contact'
                 >
             <button type="button" className="inline-flex flex-col items-center justify-center px-5 dark:hover:bg-gray-800 group">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="animate-bounce w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-red-500">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                 </svg>
