be8b36e7d9cfa01de80b9a6fddf66cd735508770
unknown <alexnoreaga@gmail.com>
2023-11-15 12:07:31 +0700

    perubahan posisi tombol live

--- app/components/LiveShopee.jsx
+++ app/components/LiveShopee.jsx
@@ -6,7 +6,7 @@ export const LiveShopee = () => {
         
         <a href="#" target="_blank" className='drop-shadow-sm text-white '>     
             <div className=' items-center bg-slate-100 rounded p-2 h-20 cursor-pointer font-semibold text-white text-center  flex flex-row gap-2'>
-                <div className='absolute flex items-center ml-7'>
+                <div className='absolute flex items-center gap-2 left-14'>
                 <div className='animate-ping absolute left-2.5 w-9 h-9 inline-flex rounded-full  bg-gradient-to-r from-fuchsia-600 to-pink-600  opacity-75'></div>
                 {/* <div className='absolute w-14 h-full animate-ping inline-flex rounded-full  bg-gradient-to-r from-fuchsia-600 to-pink-600  opacity-75'> */}
                     {/* </div> */}
@@ -14,10 +14,10 @@ export const LiveShopee = () => {
                         <img src="https://cdn.shopify.com/s/files/1/0672/3806/8470/files/logo_galaxy_profil.jpg?v=1699977908" alt="" className='relative rounded-full w-full h-auto'/>
                         <div className='absolute left-2 bottom-0 bg-gradient-to-r rounded from-fuchsia-600 to-pink-600 w-10 text-xs'>live</div>
                     </div>
-                    </div> 
-
-                    <div className='text-slate-900 m-auto'>Yuk tanya di LIVE Shopee</div>
+                    <div className='text-slate-900'>Yuk tanya di LIVE Shopee</div>
+                    </div>  
                 </div> 
+
           </a>
          
     
