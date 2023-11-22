56e8c9466a21ed77dd0efacc550b2cb1dcf0ccfc
unknown <alexnoreaga@gmail.com>
2023-11-22 18:38:21 +0700

    perubahan banner depan

--- app/components/BrandPopular.jsx
+++ app/components/BrandPopular.jsx
@@ -1,3 +1,4 @@
+import {Await, useLoaderData, Link} from '@remix-run/react';
 
 
 export const BrandPopular = ({brands}) => {
@@ -5,9 +6,14 @@ export const BrandPopular = ({brands}) => {
     console.log('Ini adalah brands',brands)
 
   return (
-    <div className="my-4">
+    <div className="my-6">
+    <div className='flex flex-row items-center justify-between m-1 mb-2'>
     <div className="text-slate-800 text-lg mx-auto sm:mx-1 px-1 whitespace-pre-wrap max-w-prose font-bold text-lead">Brand Popular</div>
-    <div className="flex flex-wrap gap-x-2 p-2 items-center shadow-md rounded-lg">
+    <Link to={`/brands/`}>
+            <div className='text-slate-500 hidden sm:block mx-1'>Lihat Selengkapnya</div>
+    </Link>
+    </div>
+    <div className="flex flex-wrap justify-center gap-x-2 p-2 items-center shadow-md rounded-lg">
         {brands.map((brand)=>{
             return(
                 <img className="w-20 sm:w-28 h-auto hover:shadow-md hover:border rounded-lg p-2" src={brand.metaobject.fields[1].reference.image.url} alt={brand.metaobject.fields[0].value}/>
