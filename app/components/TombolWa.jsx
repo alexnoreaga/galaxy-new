3571e98884f2131a0d91fc897a5d7b2cb5fd3990
unknown <alexnoreaga@gmail.com>
2023-12-01 17:29:01 +0700

    penambahan tombol wa

--- app/components/TombolWa.jsx
+++ app/components/TombolWa.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { FaWhatsapp } from "react-icons/fa6";
+
 
 export const TombolWa = () => {
   return (
     <>
-        <div className="fixed bottom-20 text-right right-6 rounded-full bg-green-400 p-2">
+        <div className="shadow-xl fixed bottom-20 text-right right-6 rounded-full bg-gradient-to-r from-green-400 to-green-600">
+            <div className='text-md flex text-white flex-row items-center gap-1 p-2'>
+            <FaWhatsapp className='text-xl'/>
             <div>Chat Admin</div>
+            </div>
         </div>
     </>
   )
