b149e6f46f271710339dfabaa7e936cad77bdc7e
unknown <alexnoreaga@gmail.com>
2023-12-07 15:43:55 +0700

    perubahan pada kolom pencarian

--- app/components/TombolWa.jsx
+++ app/components/TombolWa.jsx
@@ -5,8 +5,8 @@ import { FaWhatsapp } from "react-icons/fa6";
 export const TombolWa = () => {
   return (
     <>
-        <div className=" shadow-xl fixed bottom-20 sm:bottom-16 text-right right-0 backdrop-blur-md rounded-l-full bg-gradient-to-r from-green-400/80 to-green-700/80">
-            <div className=' text-sm md:text-md flex text-white flex-row items-center gap-1 p-2'>
+        <div className="p-2 sm:py-3 shadow-xl fixed bottom-20 sm:bottom-16 text-right right-0 backdrop-blur-md rounded-l-full bg-gradient-to-r from-green-400/80 to-green-700/80">
+            <div className=' text-sm md:text-md flex text-white flex-row items-center gap-1 '>
             <FaWhatsapp className='text-xl drop-shadow-lg'/>
             <div className='hidden sm:block drop-shadow-lg'>Chat Admin</div>
             <div className='block sm:hidden drop-shadow-lg'>Chat</div>
