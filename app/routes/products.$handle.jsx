daef35fce05a1f7a6117b22ed64995c2856faff6
unknown <alexnoreaga@gmail.com>
2024-01-29 21:44:50 +0700

    perubahan tombol

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -157,9 +157,9 @@ export async function loader({params, context, request}) {
 
 <div className='flex flex-row gap-1'>
             {!product?.metafields[12]?.value && selectedVariant?.availableForSale && (
-  <div className='w-28 p-1 items-center rounded-full border border-gray-300'>
+  <div className='w-28 p-1 items-center rounded-full border border-gray-200'>
     <div className='m-auto flex flex-row'>
-      <div className='text-sm m-auto text-gray-600'>Stock Ready</div>
+      <div className='text-sm m-auto text-gray-500'>Stock Ready</div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="m-auto w-6 h-6 text-emerald-500">
         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
       </svg>
@@ -168,9 +168,9 @@ export async function loader({params, context, request}) {
 )}
 
 {product.metafields[0]?.value && (
-  <div className='w34 p-1 items-center rounded-full border border-gray-300'>
+  <div className='w34 p-1 items-center rounded-full border border-gray-200'>
     <div className='m-auto flex flex-row'>
-      <div className='text-sm m-auto text-gray-600'>Garansi Resmi</div>
+      <div className='text-sm m-auto text-gray-500'>Garansi Resmi</div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="m-auto w-6 h-6 text-emerald-500">
         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
       </svg>
