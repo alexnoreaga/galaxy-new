6c9fdc92acce9f09b403c8850f32ff4108e5d37c
unknown <alexnoreaga@gmail.com>
2024-01-29 21:43:46 +0700

    penambahan info garansi resmi

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -155,16 +155,31 @@ export async function loader({params, context, request}) {
               </h1>
             </div>
 
+<div className='flex flex-row gap-1'>
             {!product?.metafields[12]?.value && selectedVariant?.availableForSale && (
-  <div className='gap-1 w-32 p-1 items-center rounded-full border border-gray-300'>
+  <div className='w-28 p-1 items-center rounded-full border border-gray-300'>
     <div className='m-auto flex flex-row'>
-      <div className='text-md font-semibold m-auto text-gray-600'>Stock Ready</div>
+      <div className='text-sm m-auto text-gray-600'>Stock Ready</div>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="m-auto w-6 h-6 text-emerald-500">
         <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
       </svg>
     </div>
   </div>
 )}
+
+{product.metafields[0]?.value && (
+  <div className='w34 p-1 items-center rounded-full border border-gray-300'>
+    <div className='m-auto flex flex-row'>
+      <div className='text-sm m-auto text-gray-600'>Garansi Resmi</div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="m-auto w-6 h-6 text-emerald-500">
+        <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+      </svg>
+    </div>
+  </div>
+)}
+</div>
+
+
             {/* <div className='flex flex-row gap-1 items-center text-slate-700 text-sm'>
             <div>Garansi Resmi 1 Tahun</div>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-sky-400">
