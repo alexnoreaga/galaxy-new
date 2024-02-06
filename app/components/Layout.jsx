bdb9cd9c8211aef580130e08593abddab1c70c51
unknown <alexnoreaga@gmail.com>
2024-02-06 18:29:29 +0700

    perubahan pada halaman produk

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -46,7 +46,7 @@ export function Layout({cart, children = null, footer, header, isLoggedIn,footer
       to={`/pages/contact`}
       className='no-underlined'
     >
-    <div className="flex flex-row items-center border-white border w-44 h-12 rounded-full mt-3">
+    <div className="flex flex-row items-center bg-white/30 backdrop-blur-sm  w-44 h-12 rounded-full mt-3">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="m-auto mr-2 sm:w-6 sm:h-6 w-5 h-5 text-white">
         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
