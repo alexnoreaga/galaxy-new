a51d8543f9151b8569e2768b3c90df5627c82ac0
unknown <alexnoreaga@gmail.com>
2024-02-11 13:02:20 +0700

    penambahan google tag manager

--- app/root.jsx
+++ app/root.jsx
@@ -105,6 +105,19 @@ export default function App() {
   const nonce = useNonce();
   const data = useLoaderData();
 
+  useEffect(() => {
+    // Google Tag Manager script
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];
+      w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KR7LGXFF');
+  }, []);
+
 
   // console.log(data)
 
@@ -129,6 +142,20 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+      <script defer>
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KR7LGXFF');
+          `}
+        </script>
+
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="theme-color" content="#000000"></meta>
@@ -158,6 +185,10 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR7LGXFF" height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe>
+        </noscript>
+        
         <Layout {...data}>
           <Outlet />
         </Layout>
