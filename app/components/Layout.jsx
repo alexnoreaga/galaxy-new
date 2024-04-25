cf074de76622c3cc9e7f220b1d66898651dba1ca
unknown <alexnoreaga@gmail.com>
2024-04-26 01:30:13 +0700

    berusaha

--- app/components/Layout.jsx
+++ app/components/Layout.jsx
@@ -27,6 +27,59 @@ export function Layout({cart, children = null, footer, header, isLoggedIn,footer
 
     const error = useRouteError();
 
+    if (error) {
+      return (
+          <>
+      <CartAside cart={cart} />
+      <SearchAside />
+      <MobileMenuAside menu={header.menu} />
+      
+      <Header header={header} cart={cart} isLoggedIn={isLoggedIn} />
+
+
+      {/* <Breadcrumbs /> */}
+   
+
+
+      <main>{children}</main>
+
+
+
+      <div className="h-72  mt-5 bg-gray-900 flex items-center bg-fixed md:bg-[url('https://cdn.shopify.com/s/files/1/0672/3806/8470/files/Foto-toko-tangerang-2.webp?v=1706799592')] bg-[url('https://cdn.shopify.com/s/files/1/0672/3806/8470/files/Foto-toko-tangerang-portrait.png?v=1706799750')]">
+  <div className='p-5 mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl'>
+    <h1 className='md:mb-2 mb-1 text-left text-md md:text-xl lg:text-2xl text-white'>Kunjungi Toko Kami</h1>
+    <p className="text-white text-sm">Dapatkan Penawaran Harga Terbaik Khusus Pembelian di Store Langsung</p>
+    <Link
+      to={`/pages/contact`}
+      className='no-underlined'
+    >
+    <div className="flex flex-row items-center bg-white/30 backdrop-blur-sm  w-32 h-12 rounded-full mt-3">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="m-auto mr-2  w-5 h-5 text-white">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+      </svg>
+
+      <p className='no-underline text-white ml-0 text-sm sm:text-sm md:text-sm lg:text-sm m-auto'>Lihat Maps</p>
+      </div>
+    </Link>
+  </div>
+</div>
+      <Suspense>
+        <Await resolve={footer}>
+          {(footer) => <Footer menu={footer.menu} footerSatu = {footerSatu}/>}
+        </Await>
+      </Suspense>
+      
+      {/* <BottomNavbar/> */}
+      <TombolWa/>
+      {urlSekarang.includes('cart') ? (
+        <BottomNavbar/>
+      ) : urlSekarang.includes('collections') ? (
+          <BottomNavbar/>
+      ) :  <BottomNavbar/>} 
+    </>
+      );
+  }
 
 
 
@@ -42,7 +95,7 @@ export function Layout({cart, children = null, footer, header, isLoggedIn,footer
       <Header header={header} cart={cart} isLoggedIn={isLoggedIn} />
 
 
-      {!error && <Breadcrumbs />}
+      <Breadcrumbs />
    
 
 
