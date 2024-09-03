06cc643d7b7e436db313cf6e4f7c442443e76ea3
unknown <alexnoreaga@gmail.com>
2024-09-03 16:43:56 +0700

    penambahan tiktok shop

--- app/routes/marketplace.jsx
+++ app/routes/marketplace.jsx
@@ -44,6 +44,18 @@ export default function Marketplace() {
     const feeBukalapak = 2.00
 
 
+    // BIAYA ADMIN TIKTOK MARKETPALCE
+    const feeTiktok5c75 = 5.75
+    const feeTiktok10 = 10.00
+    const feeTiktok7c5 = 7.50
+
+
+    
+    // BIAYA ADMIN TIKTOK MALL
+    const feeTiktok4 = 4.00
+    const feeTiktok8c5 = 8.5
+
+
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -114,7 +126,7 @@ const hitungAkulaku = (biayaAdm) => {
 
   const price = parseFloat(modal.price) || 0;
   const basePrice = parseFloat(modal.basePrice) || 0;
-  const biayaAdmAkulaku = (price * feeAkulaku) / 100
+  const biayaAdmAkulaku = (price * biayaAdm) / 100
 
   return ((price - biayaAdmAkulaku) - basePrice);
 }
@@ -123,9 +135,28 @@ const hitungBukalapak = (biayaAdm) => {
 
   const price = parseFloat(modal.price) || 0;
   const basePrice = parseFloat(modal.basePrice) || 0;
-  const biayaAdmBukalapak = (price * feeBukalapak) / 100
+  const biayaAdmBukalapak = (price * biayaAdm) / 100
 
   return ((price - biayaAdmBukalapak) - basePrice);
+}
+
+
+const hitungTiktokMerchant = (biayaAdm) => {
+
+  const price = parseFloat(modal.price) || 0;
+  const basePrice = parseFloat(modal.basePrice) || 0;
+  const biayaAdminTiktok1 = (price * biayaAdm) / 100
+
+  return ((price - biayaAdminTiktok1) - basePrice);
+}
+
+const hitungTiktokMall = (biayaAdm) => {
+
+  const price = parseFloat(modal.price) || 0;
+  const basePrice = parseFloat(modal.basePrice) || 0;
+  const biayaAdminTiktok1 = (price * biayaAdm) / 100
+
+  return ((price - biayaAdminTiktok1) - basePrice);
 }
 
 
@@ -186,7 +217,7 @@ const hitungBukalapak = (biayaAdm) => {
 
         )}/> */}
 
-        <div className='border rounded-lg p-2 border-green-600'>
+        <div className='border rounded-lg p-2 border-green-600 shadow-lg'>
 
           <div className='text-center'>
           <div className="font-bold text-green-600 text-xl">Tokopedia Official Store</div>
@@ -623,6 +654,137 @@ const hitungBukalapak = (biayaAdm) => {
 
 
         </div>
+
+
+
+
+
+
+
+
+        <div className='border border-gray-800 rounded-lg p-2 mt-4'>
+
+          <div className='text-center'>
+          <div className="font-bold text-gray-800 text-xl">Tiktok Shop Regular Marketplace</div>
+          {/* <div className='text-xs text-gray-500'>Biaya Layanan Official Store 1.8% (Maksimal 50rb)</div>
+          <div className='text-xs text-gray-500'>Biaya Layanan Pengiriman Official Store 2% (Maksimal 10rb)</div> */}
+
+          </div>
+
+          <div className='p-2 bg-slate-100 mt-4 rounded-md shadow-md'>
+            
+            <div className='flex items-center gap-2 text-gray-800'>
+            <div className='text-lg font-bold p-1 rounded-lg bg-gray-800 text-white'>{feeTiktok5c75}%</div>
+            <div className='text-xs font-bold underline'>Phone & Electronics</div>
+            </div>
+
+            <div className='flex flex-wrap items-center mt-2 lg:gap-2'>
+              <div className='text-xs font-bold mr-1'>Cameras & Photography</div>
+              <div className='text-xs'>Action Cameras, Drones & Accessories, DSLRs, Instant Cameras, Mirrorless Cameras, Point & Shoot Cameras, Security Cameras & SYstems, Video Camcorders</div>
+            </div>
+
+            <div className='bg-gray-200 text-gray-500 text-center font-bold rounded-md mt-2'>Potongan : Rp {((modal.price - hitungTiktokMerchant(feeTiktok5c75))- modal.basePrice)?.toLocaleString()}</div>
+            <div className='bg-gray-800 text-white text-center font-bold rounded-md mt-2'>Margin : Rp {hitungTiktokMerchant(feeTiktok5c75)?.toLocaleString()}</div>
+
+          </div>
+
+
+
+          <div className='p-2 bg-slate-100 mt-4 rounded-md shadow-md'>
+            
+            <div className='flex items-center gap-2 text-gray-800'>
+            <div className='text-lg font-bold p-1 rounded-lg bg-gray-800 text-white'>{feeTiktok7c5}%</div>
+            <div className='text-xs font-bold underline'>Phone & Electronics</div>
+            </div>
+
+            <div className='flex flex-wrap items-center mt-2 lg:gap-2'>
+              <div className='text-xs font-bold mr-1'>Cameras & Photography</div>
+              <div className='text-xs'>Camera Lenses</div>
+            </div>
+
+            <div className='bg-gray-200 text-gray-500 text-center font-bold rounded-md mt-2'>Potongan : Rp {((modal.price - hitungTiktokMerchant(feeTiktok7c5))- modal.basePrice)?.toLocaleString()}</div>
+            <div className='bg-gray-800 text-white text-center font-bold rounded-md mt-2'>Margin : Rp {hitungTiktokMerchant(feeTiktok7c5)?.toLocaleString()}</div>
+
+          </div>
+
+
+          <div className='p-2 bg-slate-100 mt-4 rounded-md shadow-md'>
+            
+            <div className='flex items-center gap-2 text-gray-800'>
+            <div className='text-lg font-bold p-1 rounded-lg bg-gray-800 text-white'>{feeTiktok10}%</div>
+            <div className='text-xs font-bold underline'></div>
+            </div>
+
+            <div className='flex flex-wrap items-center mt-2 lg:gap-2'>
+              <div className='text-xs font-bold mr-1'>Phone & Electronics</div>
+              <div className='text-xs'>Camera Accessories, Camera Care, Film Cameras</div>
+            </div>
+
+
+            <div className='bg-gray-200 text-gray-500 text-center font-bold rounded-md mt-2'>Potongan : Rp {((modal.price - hitungTiktokMerchant(feeTiktok10))- modal.basePrice)?.toLocaleString()}</div>
+            <div className='bg-gray-800 text-white text-center font-bold rounded-md mt-2'>Margin : Rp {hitungTiktokMerchant(feeTiktok10)?.toLocaleString()}</div>
+
+          </div>
+
+        </div>
+
+
+
+
+
+        <div className='border border-slate-600 rounded-lg p-2 mt-4'>
+
+          <div className='text-center'>
+          <div className="font-bold text-slate-600 text-xl">Tiktok Mall</div>
+          {/* <div className='text-xs text-gray-500'>Biaya Layanan Official Store 1.8% (Maksimal 50rb)</div>
+          <div className='text-xs text-gray-500'>Biaya Layanan Pengiriman Official Store 2% (Maksimal 10rb)</div> */}
+
+          </div>
+
+          <div className='p-2 bg-slate-100 mt-4 rounded-md shadow-md'>
+            
+            <div className='flex items-center gap-2 text-slate-600'>
+            <div className='text-lg font-bold p-1 rounded-lg bg-slate-600 text-white'>{feeTiktok4}%</div>
+            <div className='text-xs font-bold underline'>Phone & Electronics</div>
+            </div>
+
+            <div className='flex flex-wrap items-center mt-2 lg:gap-2'>
+              <div className='text-xs font-bold mr-1'>Cameras & Photography</div>
+              <div className='text-xs'>Action Cameras, Drones & Accessories, DSLRs, Instant Cameras, Mirrorless Cameras, Point & Shoot Cameras, Security Cameras & SYstems, Video Camcorders</div>
+            </div>
+
+            <div className='flex flex-wrap items-center mt-2 lg:gap-2'>
+              <div className='text-xs font-bold mr-1'>Cameras & Photography</div>
+              <div className='text-xs'>Camera Lenses</div>
+            </div>
+
+            <div className='bg-gray-200 text-gray-500 text-center font-bold rounded-md mt-2'>Potongan : Rp {((modal.price - hitungTiktokMall(feeTiktok4))- modal.basePrice)?.toLocaleString()}</div>
+            <div className='bg-slate-600 text-white text-center font-bold rounded-md mt-2'>Margin : Rp {hitungTiktokMall(feeTiktok4)?.toLocaleString()}</div>
+
+          </div>
+
+
+          <div className='p-2 bg-slate-100 mt-4 rounded-md shadow-md'>
+            
+            <div className='flex items-center gap-2 text-slate-600'>
+            <div className='text-lg font-bold p-1 rounded-lg bg-slate-600 text-white'>{feeTiktok8c5}%</div>
+            <div className='text-xs font-bold underline'>Phone & Electronics</div>
+            </div>
+
+            <div className='flex flex-wrap items-center mt-2 lg:gap-2'>
+              <div className='text-xs font-bold mr-1'>Phone & Electronics</div>
+              <div className='text-xs'>Camera Accessories, Camera Care, Film Cameras</div>
+            </div>
+
+
+            <div className='bg-gray-200 text-gray-500 text-center font-bold rounded-md mt-2'>Potongan : Rp {((modal.price - hitungTiktokMall(feeTiktok8c5))- modal.basePrice)?.toLocaleString()}</div>
+            <div className='bg-slate-600 text-white text-center font-bold rounded-md mt-2'>Margin : Rp {hitungTiktokMall(feeTiktok8c5)?.toLocaleString()}</div>
+
+          </div>
+
+        </div>
+
+
 
 
       
