d10fd046d25b3756675bc2580d926226b752e59a
unknown <alexnoreaga@gmail.com>
2024-12-14 17:00:07 +0700

    perubahan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 import formsPlugin from '@tailwindcss/forms';
 import typographyPlugin from '@tailwindcss/typography';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
-  plugins: [formsPlugin, typographyPlugin, require("flowbite/plugin"),tailwindcss, autoprefixer],
+  plugins: [formsPlugin, typographyPlugin, require("flowbite/plugin")],
   
 };
 
