5802607ca405b31545603c0482f1b03c786e5c24
unknown <alexnoreaga@gmail.com>
2024-12-14 17:38:46 +0700

    perubahan percobaan kembali

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ import typographyPlugin from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Add this line
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   plugins: [formsPlugin, typographyPlugin, require("flowbite/plugin")],
   
 };
